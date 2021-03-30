0ef885ce7f723a1ca0e27eb889661f85119a37f7
Hamar Dávid <57978467+HamarDavid@users.noreply.github.com>
2021-03-30 13:27:53 +0200

    Core/module - LanguageKnowledge.java file inicializálása

---
+++ Program/AFP2Project/Core/src/main/java/model/LanguageKnowledge.java
@@ -0,0 +1,17 @@
+package model;
+
+public class LanguageKnowledge {
+    private String language;
+    private String level;
+    private int id;
+
+    public LanguageKnowledge() {
+    }
+
+    public LanguageKnowledge(String language, String level) {
+        this.language = language;
+        this.level = level;
+    }
+
+
+}
