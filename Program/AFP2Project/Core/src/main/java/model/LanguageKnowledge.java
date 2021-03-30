3a5435cf79c9cd878ba707a7920602d735ad46f8
Hamar Dávid <57978467+HamarDavid@users.noreply.github.com>
2021-03-30 13:29:15 +0200

    Core/model - LanguageKnowledge.java - getterek és setterek lekészítése

--- Program/AFP2Project/Core/src/main/java/model/LanguageKnowledge.java
+++ Program/AFP2Project/Core/src/main/java/model/LanguageKnowledge.java
@@ -13,5 +13,28 @@ public class LanguageKnowledge {
         this.level = level;
     }
 
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
