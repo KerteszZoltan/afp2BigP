e5eeca64c09ea7fc28d1f2b49fb320b52952334f
Hamar Dávid <57978467+HamarDavid@users.noreply.github.com>
2021-04-13 10:07:12 +0200

    Core mappa - felesleges fileok eltávolítása

--- Program/AFP2Project/Core/src/main/java/exceptions/IncorrectNeptunCode.java
+++
@@ -1,4 +0,0 @@
-package exceptions;
-
-public class IncorrectNeptunCode extends Throwable {
-}
--- Program/AFP2Project/Core/src/main/java/model/Department.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum Department {
-    CS,BI,TM,ITL
-    //CS:Computer Sience, BI: Bussines Informatics, TM: Teacher Major, ITL: IT Librarian
-}
