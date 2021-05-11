cf093bce39952f5e2228a499851f4f8c8585a2f7
KerteszZoltan <58435679+KerteszZoltan@users.noreply.github.com>
2021-05-11 10:46:16 +0200

    Tesztelés: Tesztekhez generált kód, pom.xml-be generált sorok, RESTStudentController-hez a teszt java létrehozás

---
+++ Program/AFP2DemoProject/AFP2Demo/controller/src/main/java/uni/eszterhazy/project/controller/RESTStudentControllerTest.java
@@ -0,0 +1,30 @@
+package uni.eszterhazy.project.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RESTStudentControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getStudentById() {
+    }
+
+    @Test
+    void getAllStudentOfDepartment() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+}
